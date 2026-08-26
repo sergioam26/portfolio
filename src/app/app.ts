6864287f7de17ae70281a01b88e0f74b927eb863
Sergio Algarrada Miranda <sergioalgmir@gmail.com>
2026-08-26 22:31:14 +0200

    Estructura e información básica

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,8 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
+import { Hero } from './components/hero/hero';
+import { Experience } from './components/experience/experience';
+import { Projects } from './components/projects/projects';
+import { Skills } from './components/skills/skills';
+import { Footer } from './components/footer/footer';
 
 @Component({
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Hero, Experience, Projects, Skills, Footer],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
