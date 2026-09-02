9eb05f6b6ba3c3b1e38c13277c3309163417171e
Sergio Algarrada Miranda <sergioalgmir@gmail.com>
2026-09-02 18:47:52 +0200

    Corregido espacio entre secciones y añadida funcionalidad de descargar cv en inglés y español

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -7,16 +7,29 @@ import { Component, signal } from '@angular/core';
   templateUrl: './navbar.html',
 })
 export class Navbar {
-  // Estado para controlar si el menú colapsable está abierto o cerrado en móviles
+  // Control del menú hamburguesa móvil
 
   protected readonly isMenuOpen = signal(false);
+
+  // Control del desplegable de idiomas del CV
+
+  protected readonly isCvMenuOpen = signal(false);
 
   protected toggleMenu(): void {
     this.isMenuOpen.update((open) => !open);
   }
 
-  protected closeMenu(): void{
-      this.isMenuOpen.set(false);
+  protected closeMenu(): void {
+    this.isMenuOpen.set(false);
+    this.isCvMenuOpen.set(false);
   }
 
+  protected toggleCvMenu(event: Event): void {
+    event.stopPropagation();
+    this.isCvMenuOpen.update((open) => !open);
+  }
+
+  protected closeCvMenu(): void {
+    this.isCvMenuOpen.set(false);
+  }
 }
